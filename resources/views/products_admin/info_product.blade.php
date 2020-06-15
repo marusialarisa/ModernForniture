217854f0ab68c651fc9f11dd9763a413c15cac13
marusialarisa <61754328+marusialarisa@users.noreply.github.com>
2020-06-15 23:06:32 +0200

    Update info_product.blade.php

--- resources/views/products_admin/info_product.blade.php
+++ resources/views/products_admin/info_product.blade.php
@@ -45,7 +45,7 @@
 
                             @foreach($photos as $photo)
                                 <div class="carousel-item {{ $loop->first ? ' active' : '' }}">
-                                    <img id=" {{$tam = $tam + 1}}" src="{{secure_asset('storage/'.$photo->photo)}}" class="element">
+                                    <img id=" {{$tam = $tam + 1}}" src="{{asset('storage/'.$photo->photo)}}" class="element">
                                     <p class="slider-n">{{$tam ?? ''}}/{{$total}}</p>
                                     <p>  {{$photo->id}} </p></div>
                             @endforeach
@@ -77,33 +77,6 @@
 
                     </div>
         </div>
-
-        <div id="derecha">
-            <h1 class="titulogandreinformacion"><strong>Información producto</strong></h1><br/>
-            @csrf
-
-
-            @csrf
-            @method('PUT')
-            <table class="table_info">
-                <thead>
-
-                <tr>
-                    <td class="textoizquierda">
-                        <strong>Nombre:   </strong> {{$producto->nombre_producto}}
-                        <br/> <strong>Marca:   </strong> {{$producto->marca}}
-                        <br/> <strong>Tipo mueble:   </strong> {{$producto->tipo_mueble}}
-                        <br/> <strong>Descripción:   </strong> {{$producto->descripcion}}
-                        <br/> <strong>Dimensiones:   </strong> {{$producto->dimensiones}}
-                        <br/> <strong>Volum:   </strong> {{$producto->volum}}
-                        <br/> <strong>Oferta:   </strong> {{$producto->oferta}}
-                        <br/> <strong>Cantidad:   </strong> {{$producto->cantidad}}
-                        <br/> <strong>Precio:   </strong> {{$producto->price}}
-                        <br/> <strong>Precio con montaje:   </strong> {{$producto->precio_con_montaje}}
-                        <br/> <strong>Fragil:   </strong> {{$producto->fragil}}
-                    </td>
-                </tr>
-
 
         <br/>  <br/>
     </div>
